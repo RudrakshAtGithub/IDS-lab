69707c9bffff0a91a8a21188b9fbff0cebcae144
Rudraksh Joshi <119342758+RudrakshAtGithub@users.noreply.github.com>
2023-08-09 15:02:29 +0530

    added 1st question of 1st assignment.

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+#include<string.h>
+
+int main(){
+	int i,count=0;
+	char str[]="hellonigga!");
+	while (str[i] !=0 ){
+		count++
+		i++;
+	}
+	printf("lenght of string is: %d \n",count);
+	return 0;
+}
